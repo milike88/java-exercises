532051630ccf30d443b71ac5bef806920e9ed450
Milan Kovacs <hqk903@gmail.com>
2023-02-28 21:06:52 +0100

    update2.1

--- src/com/busa/Main.java
+++ src/com/busa/Main.java
@@ -5,30 +5,33 @@ public class Main{
            public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
 
+            System.out.print("Input sign: ");
+            String sign = in.toString();
+            
             System.out.print("Input first number: ");
             int num1 = in.nextInt();
 
             System.out.print("Input second number: ");
             int num2 = in.nextInt();
 
-            System.out.print("Input sign: ");
-            Scanner sign = new Scanner(System.in);
 
-            if (sign=+) {
+
+
+            if (sign.equals("+")) {
                 System.out.println(num1 + " + " + num2 + " = " +
                         (num1 + num2));
-            } else if (sign=-) {
+            } else if (sign.equals("-")) {
                 System.out.println(num1 + " - " + num2 + " = " +
                     (num1 - num2));
-            } else if (sign=*) {
+            } else if (sign.equals("*")) {
                 System.out.println(num1 + " x " + num2 + " = " +
                            (num1 * num2));
             }
-            if (sign=/) {
+            if (sign.equals("/")) {
                    System.out.println(num1 + " / " + num2 + " = " +
                            (num1 / num2));
                }
-            if (sign=%){
+            if (sign.equals("%")){
                 System.out.println(num1 + " mod " + num2 + " = " +
                            (num1 % num2));
                }
